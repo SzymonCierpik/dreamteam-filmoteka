759cd93c8998c467c742e6ea54342762d141772d
MarcinZbor <marcinzb@gazeta.pl>
2023-04-20 18:32:48 +0200

    Adding dark mode

--- src/index.js
+++ src/index.js
@@ -78,3 +78,18 @@ function clearGallery() {
 }
 
 // INPUT - END//
+
+
+// Dark Mode 
+
+const body = document.querySelector("body");
+const icon = document.querySelector(".fa-regular");
+const switchBtn = document.querySelector("#toggle__checkbox");
+
+switchBtn.addEventListener("click", () => {
+  body.classList.toggle("dark__theme");
+  icon.classList.toggle("fa-sun");
+  icon.classList.toggle("fa-moon");
+});
+
+// Dark Mode End
