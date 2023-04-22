5e4316a34791664ed306a4c1869de8a2f8af302a
Szymon Cierpik <109699547+SzymonCierpik@users.noreply.github.com>
2023-04-22 19:44:09 +0200

    Update loader.js

--- src/JS/loader.js
+++ src/JS/loader.js
@@ -1,7 +1,5 @@
 window.addEventListener('load', () => {
   const loader = document.querySelector('.loader');
   loader.classList.add('loader-hidden');
-  loader.addEventListener('transitionend', () => {
-    document.body.removeChild('loader');
-  });
+  loader.addEventListener('transitionend', () => {});
 });
