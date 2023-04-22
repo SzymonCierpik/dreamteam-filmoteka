1c068e7124539b8bfc7bdb35af1699b0d54fafe4
Szymon Cierpik <109699547+SzymonCierpik@users.noreply.github.com>
2023-04-22 07:49:58 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import './JS/loader.js';
-import { getMovies, getPopularMovies, getGenresMovies } from './JS/getMovies';
+import './js/loader.js';
+import { getMovies, getPopularMovies, getGenresMovies } from './js/getMovies';
 // MOST POPULAR MOVIES - START //
 window.onload = async () => {
   const data = await getPopularMovies();
