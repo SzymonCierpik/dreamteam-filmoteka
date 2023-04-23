61c7488590d7b10c2d73147bd0b1b65173c4144e
Szymon Cierpik <109699547+SzymonCierpik@users.noreply.github.com>
2023-04-23 22:29:14 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 import './JS/loader.js';
 import './JS/renderingPopularMovies.js';
 import './JS/renderingMoviesByKey.js';
-
-// Dark Mode
-import './js/darkmode.js'
-// Dark Mode End
+import './JS/darkmode.js';
