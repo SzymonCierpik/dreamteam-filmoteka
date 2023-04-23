d622a81368b72806935c0686e8bbbd67eec162be
lubkepatryk <lubke.patryk@gmail.com>
2023-04-23 17:51:58 +0200

    bug fix completed, footer background color updated for dark mode

--- src/index.js
+++ src/index.js
@@ -127,11 +127,13 @@ function clearGallery() {
 const body = document.querySelector('body');
 const icon = document.querySelector('.fa-regular');
 const switchBtn = document.querySelector('#toggle__checkbox');
+const footer = document.querySelector('.footer');
 
 switchBtn.addEventListener('click', () => {
   body.classList.toggle('dark__theme');
   icon.classList.toggle('fa-sun');
   icon.classList.toggle('fa-moon');
+footer.classList.toggle('dark__theme');
 });
 
 // Dark Mode End
