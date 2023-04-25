438bd8526a1eec28c95e88aa26a9fd7f346b0124
Mykola Kovalskyi <112631611+mykolakovalskyi@users.noreply.github.com>
2023-04-25 23:45:54 +0200

    comit

--- src/JS/modalMarkup.js
+++ src/JS/modalMarkup.js
@@ -9,49 +9,56 @@ function renderMarkup({
   vote_count,
   genres,
 }) {
-  let poster = URL_IMG + poster_path;
   if (!poster_path) {
     poster = 'https://live.staticflickr.com/65535/52673964597_7ac974f3b4_k.jpg';
   }
   const markup = `<div class="movie-info-content">
-      <img class="movie-detail__image" src="https://image.tmdb.org/t/p/w500//${poster_path}" />
+      <img
+        class="movie-detail__image"
+        src="https://image.tmdb.org/t/p/w500//${poster_path}"
+      />
       <div class="modal__content">
-        <h2 class="modal__title">${original_title}</h2>
+        <h2 class="modal__title">${title}</h2>
         <ul class="modal__list">
           <li class="modal__item">
             <p class="modal__item--label">Vote / Votes</p>
             <div class="modal__item--vote">
               <p class="modal__item--value modal__item--value--vote">
-                <vote_average>${vote_average.toFixed(
-                  1
-                )}</vote_average>/<vote__count>${vote_count}</vote__count>
-              </p></div></li>
-              <li class="modal__item">
-                <p class="modal__item--label">Popularity</p>
-                <p class="modal__item--value">${popularity.toFixed(1)}</p>
-              </li>
-              <li class="modal__item">
-                <p class="modal__item--label">Original Title</p>
-                <p class="modal__item--value big">${original_title}</p>
-              </li>
-              <li class="modal__item">
-                <p class="modal__item--label">Genre</p>
-                <p class="modal__item--value">${genres}</p>
-</li>
-</ul>
-<div class="modal__overview">
-<h3 class="modal__overview--about">ABOUT</h3>
-<p class="modal__overview--overview">${overview}</p>
-</div>
-<div class="modal__buttons">
-<button id="watched" class="modal__button modal__button--watched">Add to watched</button>
-<button id="queue" class="modal__button modal__button--queue">Add to queue</button>
-</div>
-</div>
-</div>
-</div>
+                <p>${vote_average.toFixed(1)}</p>
+                /<p>${vote_count}</p>
+              </p>
+            </div>
+          </li>
+          <li class="modal__item">
+            <p class="modal__item--label">Popularity</p>
+            <p class="modal__item--value">${popularity.toFixed(1)}</p>
+          </li>
+          <li class="modal__item">
+            <p class="modal__item--label">Original Title</p>
+            <p class="modal__item--value big">${original_title}</p>
+          </li>
+          <li class="modal__item">
+            <p class="modal__item--label">Genre</p>
+            <p class="modal__item--value">${genres}</p>
+          </li>
+        </ul>
+        <div class="modal__overview">
+          <h3 class="modal__overview--about">ABOUT</h3>
+          <p class="modal__overview--overview">${overview}</p>
+        </div>
+        <div class="modal__buttons">
+          <button id="watched" class="modal__button modal__button--watched">
+            Add to watched
+          </button>
+          <button id="queue" class="modal__button modal__button--queue">
+            Add to queue
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>`;
-  return backdrop.innerHTML('beforeend', markup);
+  return markup;
 }
 
 export { renderModal };
--- src/JS/modalMovie.js
+++ src/JS/modalMovie.js
@@ -12,12 +12,13 @@ const refs = {
 refs.openModalBtn.addEventListener('click', toggleModal);
 refs.closeModalBtn.addEventListener('click', toggleModal);
 
-function toggleModal() {
+function toggleModal(e) {
   refs.modal.classList.toggle('is-hidden');
+  openModal(e);
 }
 // })();
 const openModal = e => {
-  if (!e.target.classList.contains('film-card')) {
+  if (!e.currentTarget.classList.contains('film-card')) {
     // zakanczamy dzialanie funkcji, kliknelismy nie w karte
     return;
   }
@@ -25,11 +26,29 @@ const openModal = e => {
   //   // kliknielismy w karte, dzialamy dalej
 
   //   // otrzymujemy id z data-id atrybutu, ktory zostal dodany wczesniej do karty
-  const id = e.target.dataset.id;
+  const id = e.currentTarget.dataset.id;
   // pobranie id filmu z atrybutu "data-id" klikniętego elementu
-  const movieId = e.target.closest('.film-card').getAttribute('data-id');
+  const movieId = e.currentTarget.closest('.film-card').getAttribute('data-id');
+  const backdrop = document.querySelector('.backdrop');
 
   fetchMovieById(movieId)
+    .then(response => {
+      console.log(response);
+      if (!response.ok) {
+        if (response.status === 429) {
+          // Too Many Requests
+          console.error('Error: Too many requests to API.');
+        } else {
+          console.error(
+            'Error: Failed to fetch data from API. Status code:',
+            response.status
+          );
+        }
+        throw new Error(response.status);
+      }
+      const movieData = response.json();
+      return movieData;
+    })
     .then(movieData => {
       backdrop.innerHTML = renderModal(movieData);
 
@@ -37,36 +56,20 @@ const openModal = e => {
       document.body.appendChild(backdrop);
     })
     .catch(error => {
-      console.error(error);
+      throw new Error(error);
     });
 
   const fetchMovieById = async id => {
     const API_URL = 'https://api.themoviedb.org/3/movie/';
     const API_KEY = 'b118f38ec77100db6763b4cc83604589';
-    const FETCH_URL = `${API_URL}${id}?api_key=${API_KEY}`;
+    const FETCH_URL = `${API_URL}` + `${id}` + '?api_key=' + `${API_KEY}`;
 
-    return await fetch(FETCH_URL)
-      .then(response => {
-        if (!response.ok) {
-          if (response.status === 429) {
-            // Too Many Requests
-            console.error('Error: Too many requests to API.');
-          } else {
-            console.error(
-              'Error: Failed to fetch data from API. Status code:',
-              response.status
-            );
-          }
-          throw new Error(response.status);
-        }
-        return response.json();
-      })
-      .catch(error => {
-        throw new Error(error);
-      });
+    const response = await fetch(FETCH_URL);
+    return response;
   };
 
   // szukamy nasza liste w DOM
   const galleryListDOM = document.querySelector('.films-cards-set'); // tu powinna by klasa calej galerii/listy
 };
+
 export { openModal };
