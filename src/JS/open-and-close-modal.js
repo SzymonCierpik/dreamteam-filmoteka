2a82194595c91e38183cc30605e8e65d68348158
Mykola Kovalskyi <112631611+mykolakovalskyi@users.noreply.github.com>
2023-04-25 22:22:54 +0200

    team modal added

--- src/JS/open-and-close-modal.js
+++ src/JS/open-and-close-modal.js
@@ -1,35 +1,32 @@
-import { openModal } from "./modalMovie";
+import { openModal } from './modalMovie';
 
 function toggleModal() {
-  modal.classList.toggle("is-hidden");
-  modal.classList.toggle("modal-animation")
+  modal.classList.toggle('is-hidden');
+  modal.classList.toggle('modal-animation');
 }
 
-
-
 function onCloseModal() {
-    document.body.classList.remove('is-hidden');
-  
-    modal.innerHTML = '';
-   
-  }
-  
-  function onBackDropClick(event) {
-    if (event.currentTarget === event.target) {
-      onCloseModal();
-    }
-  }
-  
-  function onEscKeyPress(event) {
-    if (event.code !== 'Escape') {
-      return;
-    }
-  
-    window.removeEventListener('keydown', onEscKeyPress);
+  document.body.classList.remove('is-hidden');
+
+  modal.innerHTML = '';
+}
+
+function onBackDropClick(event) {
+  if (event.currentTarget === event.target) {
     onCloseModal();
   }
+}
+
+function onEscKeyPress(event) {
+  if (event.code !== 'Escape') {
+    return;
+  }
+
+  window.removeEventListener('keydown', onEscKeyPress);
+  onCloseModal();
+}
 
 // Trzeba nazwy guzikow odpowiednie przepisac ja dalem tutaj przykladowe
-openModalBtn.addEventListener('click', openModal);
-closeModalBtn.addEventListener('click', onCloseModal);
-divBackdrop.addEventListener('click', onBackDropClick);
+///openModalBtn.addEventListener('click', openModal);
+///closeModalBtn.addEventListener('click', onCloseModal);
+///divBackdrop.addEventListener('click', onBackDropClick);
