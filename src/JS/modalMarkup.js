8fed52a63ad6b234b85329f780aeabb0478c3291
Mykola Kovalskyi <112631611+mykolakovalskyi@users.noreply.github.com>
2023-04-25 22:54:05 +0200

    updata

--- src/JS/modalMarkup.js
+++ src/JS/modalMarkup.js
@@ -1,40 +1,38 @@
-
-
 function renderMarkup({
+  title,
+  original_title,
+  overview,
   popularity,
   poster_path,
-  genres,
-  overview,
-  original_title,
+  id,
   vote_average,
   vote_count,
+  genres,
 }) {
   let poster = URL_IMG + poster_path;
   if (!poster_path) {
     poster = 'https://live.staticflickr.com/65535/52673964597_7ac974f3b4_k.jpg';
   }
-  const markup=
-
-  `<div class="movie-info-content">
-      <img class="movie-detail__image" src="https://image.tmdb.org/t/p/w500//${
-        e.poster_path
-      }" />
+  const markup = `<div class="movie-info-content">
+      <img class="movie-detail__image" src="https://image.tmdb.org/t/p/w500//${poster_path}" />
       <div class="modal__content">
-        <h2 class="modal__title">${e.original_title}</h2>
+        <h2 class="modal__title">${original_title}</h2>
         <ul class="modal__list">
           <li class="modal__item">
             <p class="modal__item--label">Vote / Votes</p>
             <div class="modal__item--vote">
               <p class="modal__item--value modal__item--value--vote">
-                <vote_average>${e.vote_average.toFixed(1)}</vote_average>/<vote__count>${e.vote_count}</vote__count>
+                <vote_average>${vote_average.toFixed(
+                  1
+                )}</vote_average>/<vote__count>${vote_count}</vote__count>
               </p></div></li>
               <li class="modal__item">
                 <p class="modal__item--label">Popularity</p>
-                <p class="modal__item--value">${e.popularity.toFixed(1)}</p>
+                <p class="modal__item--value">${popularity.toFixed(1)}</p>
               </li>
               <li class="modal__item">
                 <p class="modal__item--label">Original Title</p>
-                <p class="modal__item--value big">${e.original_title}</p>
+                <p class="modal__item--value big">${original_title}</p>
               </li>
               <li class="modal__item">
                 <p class="modal__item--label">Genre</p>
@@ -43,7 +41,7 @@ function renderMarkup({
 </ul>
 <div class="modal__overview">
 <h3 class="modal__overview--about">ABOUT</h3>
-<p class="modal__overview--overview">${e.overview}</p>
+<p class="modal__overview--overview">${overview}</p>
 </div>
 <div class="modal__buttons">
 <button id="watched" class="modal__button modal__button--watched">Add to watched</button>
@@ -52,10 +50,8 @@ function renderMarkup({
 </div>
 </div>
 </div>
-</div>`
-return backdrop.innerHTML('beforeend', markup);
-
+</div>`;
+  return backdrop.innerHTML('beforeend', markup);
 }
- 
-  
-  export { renderModal }; 
+
+export { renderModal };
--- src/JS/modalMovie.js
+++ src/JS/modalMovie.js
@@ -1,54 +1,51 @@
 import './open-and-close-modal';
-import { renderModal } from './modalMarkup'
-
-
-
+import { renderModal } from './modalMarkup';
 
 // funkcja otwierająca moda
 // (() => {
-  const refs = {
-    openModalBtn: document.querySelector('.films-cards-set'),
-    closeModalBtn: document.querySelector('.modal__cross-btn'),
-      modal: document.querySelector('.backdrop'),
-  };
+const refs = {
+  openModalBtn: document.querySelector('.films-cards-set'),
+  closeModalBtn: document.querySelector('.modal__cross-btn'),
+  modal: document.querySelector('.backdrop'),
+};
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+refs.openModalBtn.addEventListener('click', toggleModal);
+refs.closeModalBtn.addEventListener('click', toggleModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-  }
+function toggleModal() {
+  refs.modal.classList.toggle('is-hidden');
+}
 // })();
 const openModal = e => {
- 
   if (!e.target.classList.contains('film-card')) {
     // zakanczamy dzialanie funkcji, kliknelismy nie w karte
     return;
   }
 
-//   // kliknielismy w karte, dzialamy dalej
+  //   // kliknielismy w karte, dzialamy dalej
 
-//   // otrzymujemy id z data-id atrybutu, ktory zostal dodany wczesniej do karty
+  //   // otrzymujemy id z data-id atrybutu, ktory zostal dodany wczesniej do karty
   const id = e.target.dataset.id;
   // pobranie id filmu z atrybutu "data-id" klikniętego elementu
   const movieId = e.target.closest('.film-card').getAttribute('data-id');
+
   fetchMovieById(movieId)
     .then(movieData => {
       backdrop.innerHTML = renderModal(movieData);
 
       // dodanie elementu backdrop do ciała dokumentu
       document.body.appendChild(backdrop);
-
-          })
+    })
     .catch(error => {
       console.error(error);
     });
 
-const fetchMovieById = id => {
-  const API_URL = 'https://api.themoviedb.org/3/';
-  const API_KEY = 'b118f38ec77100db6763b4cc83604589';
-  
-    return fetch(API_URL)
+  const fetchMovieById = async id => {
+    const API_URL = 'https://api.themoviedb.org/3/movie/';
+    const API_KEY = 'b118f38ec77100db6763b4cc83604589';
+    const FETCH_URL = `${API_URL}${id}?api_key=${API_KEY}`;
+
+    return await fetch(FETCH_URL)
       .then(response => {
         if (!response.ok) {
           if (response.status === 429) {
@@ -65,12 +62,11 @@ const fetchMovieById = id => {
         return response.json();
       })
       .catch(error => {
-        throw new Error(error)
+        throw new Error(error);
       });
   };
 
-
-// szukamy nasza liste w DOM
-const galleryListDOM = document.querySelector(".films-cards-set") // tu powinna by klasa calej galerii/listy
-}
+  // szukamy nasza liste w DOM
+  const galleryListDOM = document.querySelector('.films-cards-set'); // tu powinna by klasa calej galerii/listy
+};
 export { openModal };
