3ce2838b7abf096939470cdf941310c37287f6c2
Szymon Cierpik <109699547+SzymonCierpik@users.noreply.github.com>
2023-06-14 15:32:05 +0200

    Delete src/JS directory

--- src/JS/loader.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('load', () => {
-  const loader = document.querySelector('.loader');
-  loader.classList.add('loader-hidden');
-  loader.addEventListener('transitionend', () => {});
-});
